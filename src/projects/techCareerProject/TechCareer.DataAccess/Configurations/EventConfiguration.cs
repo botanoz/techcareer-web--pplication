ab57c134f35704e194e93787d16a25427ac824c6
Botan Özalp <botanozalp@gmail.com>
2024-11-27 01:26:43 +0300

    Initial project setup: Added entities, entity configurations, and DbContext implementation

---
+++ src/projects/techCareerProject/TechCareer.DataAccess/Configurations/EventConfiguration.cs
@@ -0,0 +1,36 @@
+﻿using Core.Security.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace TechCareer.DataAccess.Configurations;
+
+public class EventConfiguration : IEntityTypeConfiguration<Event>
+{
+    public void Configure(EntityTypeBuilder<Event> builder)
+    {
+        builder.ToTable("Events").HasKey(e => e.Id);
+
+        builder.Property(e => e.Id).HasColumnName("Id").IsRequired();
+        builder.Property(e => e.Title)
+            .HasColumnName("Title")
+            .HasMaxLength(100)
+            .IsRequired();
+        builder.Property(e => e.Description)
+            .HasColumnName("Description")
+            .HasMaxLength(250);
+        builder.Property(e => e.ImageUrl)
+            .HasColumnName("ImageUrl")
+            .HasMaxLength(250);
+        builder.Property(e => e.StartDate).HasColumnName("StartDate").HasColumnType("datetime");
+        builder.Property(e => e.EndDate).HasColumnName("EndDate").HasColumnType("datetime");
+        builder.Property(e => e.ApplicationDeadline).HasColumnName("ApplicationDeadline").HasColumnType("datetime");
+        builder.Property(e => e.ParticipationText)
+            .HasColumnName("ParticipationText")
+            .HasMaxLength(250);
+
+        builder.HasOne(e => e.Category)
+            .WithMany(c => c.Events)
+            .HasForeignKey(e => e.CategoryId)
+            .OnDelete(DeleteBehavior.ClientSetNull);
+    }
+}
---
+++ src/projects/techCareerProject/TechCareer.DataAccess/Configurations/VideoEducationConfiguration.cs
@@ -0,0 +1,37 @@
+﻿using Core.Security.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace TechCareer.DataAccess.Configurations;
+
+public class VideoEducationConfiguration : IEntityTypeConfiguration<VideoEducation>
+{
+    public void Configure(EntityTypeBuilder<VideoEducation> builder)
+    {
+        builder.ToTable("VideoEducations").HasKey(ve => ve.Id);
+
+        builder.Property(ve => ve.Id).HasColumnName("Id").IsRequired();
+        builder.Property(ve => ve.Title)
+            .HasColumnName("Title")
+            .HasMaxLength(100)
+            .IsRequired();
+        builder.Property(ve => ve.Description)
+            .HasColumnName("Description")
+            .HasMaxLength(250);
+        builder.Property(ve => ve.ImageUrl)
+            .HasColumnName("ImageUrl")
+            .HasMaxLength(250);
+        builder.Property(ve => ve.TotalHour).HasColumnName("TotalHour").IsRequired();
+        builder.Property(ve => ve.IsCertified).HasColumnName("IsCertified").IsRequired();
+        builder.Property(ve => ve.Level).HasColumnName("Level").IsRequired();
+        builder.Property(ve => ve.ProgrammingLanguage)
+            .HasColumnName("ProgrammingLanguage")
+            .HasMaxLength(50)
+            .IsRequired();
+
+        builder.HasOne(ve => ve.Instructor)
+            .WithMany(i => i.VideoEducations)
+            .HasForeignKey(ve => ve.InstructorId)
+            .OnDelete(DeleteBehavior.ClientSetNull);
+    }
+}
