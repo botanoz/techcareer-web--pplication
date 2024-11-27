25f585997c8780dfb135bbb9bd53a243688d0075
Zeynep <azyildizeli@gmail.com>
2024-11-27 22:08:22 +0300

    Courses, Events and Jobs dtos are created.

---
+++ src/core/Core.Security/Dtos/Events/EventDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Core.Security.Dtos.Event;
+
+public class EventDto
+{
+    public string Id { get; set; } = string.Empty;
+    public string? Base64Image { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public List<string> Tags { get; set; }
+    public DateTime Deadline { get; set; }
+}
