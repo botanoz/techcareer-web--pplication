fe03fb8423f650461b659148c005460fd05fcb65
Yelda <yeldakuru99@outlook.com>
2024-11-29 14:33:41 +0300

    Split SeedData into smaller classes

---
+++ src/core/Core.Security/SeedData/CategorySeedData.cs
@@ -0,0 +1,25 @@
+﻿using Core.Security.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Security.SeedData
+{
+    public class CategorySeedData
+    {
+        public static List<Category> GetSeedData()
+        {
+            return new List<Category>()
+            {
+             new Category {Id=1,Name = "Bootcamp"  },
+             new Category {Id=2,Name = "Hackathon" },
+             new Category {Id=3,Name = "Hiring Challenge"},
+             new Category {Id=4,Name = "Tüm Etkinlikler" },
+            };
+
+
+        }
+    }
+}
