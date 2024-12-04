04e590a5c0bda3ccbdbe4b0aedb616b85d271160
Ahmet Tekinalp <138679697+ahmettekinalp@users.noreply.github.com>
2024-12-04 17:15:51 +0300

    IVideoEducationService and VideoEducationService have been redefined, and VideoEducationController has been added.

---
+++ src/projects/techCareerProject/TechCareer.API/Controllers/VideoEducationController.cs
@@ -0,0 +1,115 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Linq.Expressions;
+using System.Threading;
+using System.Threading.Tasks;
+using TechCareer.Service.Abstracts;
+using Core.Security.Entities;
+
+namespace TechCareer.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VideoEducationController : ControllerBase
+    {
+        private readonly IVideoEducationService _videoEducationService;
+
+        public VideoEducationController(IVideoEducationService videoEducationService)
+        {
+            _videoEducationService = videoEducationService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll(
+            [FromQuery] int index = 0,
+            [FromQuery] int size = 10,
+            [FromQuery] bool include = false,
+            [FromQuery] bool withDeleted = false,
+            [FromQuery] bool enableTracking = true,
+            CancellationToken cancellationToken = default)
+        {
+            var videoEducations = await _videoEducationService.GetPaginateAsync(
+                predicate: null,
+                orderBy: null,
+                include: include,
+                index: index,
+                size: size,
+                withDeleted: withDeleted,
+                enableTracking: enableTracking,
+                cancellationToken: cancellationToken
+            );
+
+            return Ok(videoEducations);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id, CancellationToken cancellationToken = default)
+        {
+            var videoEducation = await _videoEducationService.GetAsync(
+                predicate: x => x.Id == id,
+                include: true,
+                withDeleted: false,
+                enableTracking: true,
+                cancellationToken: cancellationToken
+            );
+
+            if (videoEducation == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(videoEducation);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] VideoEducation videoEducation)
+        {
+            if (videoEducation == null)
+            {
+                return BadRequest("VideoEducation is required.");
+            }
+
+            var createdVideoEducation = await _videoEducationService.AddAsync(videoEducation);
+
+            return CreatedAtAction(nameof(GetById), new { id = createdVideoEducation.Id }, createdVideoEducation);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id, [FromBody] VideoEducation videoEducation)
+        {
+            if (videoEducation == null || videoEducation.Id != id)
+            {
+                return BadRequest("Invalid video education data.");
+            }
+
+            var updatedVideoEducation = await _videoEducationService.UpdateAsync(videoEducation);
+
+            if (updatedVideoEducation == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(updatedVideoEducation);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id, [FromQuery] bool permanent = false)
+        {
+            var videoEducation = await _videoEducationService.GetAsync(
+                predicate: x => x.Id == id,
+                include: true,
+                withDeleted: false,
+                enableTracking: true
+            );
+
+            if (videoEducation == null)
+            {
+                return NotFound();
+            }
+
+            var deletedVideoEducation = await _videoEducationService.DeleteAsync(videoEducation, permanent);
+
+            return Ok(deletedVideoEducation);
+        }
+    }
+}
--- src/projects/techCareerProject/TechCareer.Service/Abstracts/IVideoEducationService.cs
+++ src/projects/techCareerProject/TechCareer.Service/Abstracts/IVideoEducationService.cs
@@ -1,10 +1,11 @@
 ﻿using Core.Persistence.Extensions;
+using Core.Persistence.Repositories;
 using Core.Security.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
-using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace TechCareer.Service.Abstracts
@@ -12,36 +13,35 @@ namespace TechCareer.Service.Abstracts
     public interface IVideoEducationService
     {
         Task<VideoEducation?> GetAsync(
-   Expression<Func<VideoEducation, bool>> predicate,
-   bool include = false,
-   bool withDeleted = false,
-   bool enableTracking = true,
-   CancellationToken cancellationToken = default
-);
+            Expression<Func<VideoEducation, bool>> predicate,
+            bool include = false,
+            bool withDeleted = false,
+            bool enableTracking = true,
+            CancellationToken cancellationToken = default
+        );
 
-
-        Task<Paginate<VideoEducation>> GetPaginateAsync(Expression<Func<VideoEducation, bool>>? predicate = null,
+        Task<Paginate<VideoEducation>> GetPaginateAsync(
+            Expression<Func<VideoEducation, bool>>? predicate = null,
             Func<IQueryable<VideoEducation>, IOrderedQueryable<VideoEducation>>? orderBy = null,
             bool include = false,
             int index = 0,
             int size = 10,
             bool withDeleted = false,
             bool enableTracking = true,
-            CancellationToken cancellationToken = default);
+            CancellationToken cancellationToken = default
+        );
 
-
-        Task<List<VideoEducation>> GetListAsync(Expression<Func<VideoEducation, bool>>? predicate = null,
+        Task<List<VideoEducation>> GetListAsync(
+            Expression<Func<VideoEducation, bool>>? predicate = null,
             Func<IQueryable<VideoEducation>, IOrderedQueryable<VideoEducation>>? orderBy = null,
             bool include = false,
             bool withDeleted = false,
             bool enableTracking = true,
-            CancellationToken cancellationToken = default);
-
+            CancellationToken cancellationToken = default
+        );
 
         Task<VideoEducation> AddAsync(VideoEducation videoEducation);
         Task<VideoEducation> UpdateAsync(VideoEducation videoEducation);
         Task<VideoEducation> DeleteAsync(VideoEducation videoEducation, bool permanent = false);
-
     }
 }
-
