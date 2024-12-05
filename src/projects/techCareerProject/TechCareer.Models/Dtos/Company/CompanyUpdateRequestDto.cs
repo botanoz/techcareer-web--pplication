4318fd8a00b344fc70f0b07f78d6bd7d8d77f093
Zeynep <azyildizeli@gmail.com>
2024-12-05 23:10:16 +0300

    Company dtos and controller has been updated

--- src/projects/techCareerProject/TechCareer.Models/Dtos/Company/CompanyUpdateRequestDto.cs
+++ src/projects/techCareerProject/TechCareer.Models/Dtos/Company/CompanyUpdateRequestDto.cs
@@ -4,4 +4,6 @@ public class CompanyUpdateRequestDto
 {
     public string Name { get; set; }
     public string Location { get; set; }
+    public string? ImageUrl { get; set; }
+    public string Description { get; set; }
 }
